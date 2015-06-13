31d50d219d7753506125530a672a00dfb531ca1b
KatieJia <kjia@bayviewglen.ca>
2015-06-13 01:38:20 -0400

    serialization works

--- Zork Project/src/com/bayviewglen/ZorkProject/Intro.java
+++ Zork Project/src/com/bayviewglen/ZorkProject/Intro.java
@@ -1,4 +1,6 @@
 package com.bayviewglen.ZorkProject;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.Serializable;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -9,46 +11,57 @@ import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 @SuppressWarnings("serial")
-public class Intro extends Game implements Serializable {
+public class Intro implements Serializable {
 	
 
 	@SuppressWarnings({ "resource", "unchecked" })
-	public boolean gameIntro() {
+	public Game gameIntro() throws FileNotFoundException {
 		Boolean done = false; 
+		Boolean loadGame = false;
 		while(!done){
 			System.out.println("would you like to load the previous save, or start a new game?");
 		String Answer = "";
-        BufferedReader reader = 
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             Answer = reader.readLine();
         }
         catch(java.io.IOException exc) {
-            System.out.println ("There was an error during reading: "
-                                + exc.getMessage());
+            System.out.println ("There was an error during reading: " + exc.getMessage());
         }
         if(Answer.equals("load") || Answer.equals("load game")){
         	System.out.println("your previous save has been loaded");
+        	Game game = readObjects();
+        	return game;
+       	                	          
         	
-       	                	try {
-             		   FileInputStream f_in = new FileInputStream("data/savefile.dat");
-             		    ObjectInputStream ois = new ObjectInputStream(f_in);
-             		  Object[] playerData = (Object[])ois.readObject();
-             		 currentRoom=(Room)playerData[0];
-             		//  inventory=(ArrayList<String>)playerData[1];
-             		  masterRoomMap=(HashMap<String, Room>)playerData[1];
-             		
-             		  } catch (Exception ex) {
-             		   // TODO Auto-generated catch block
-             		   ex.printStackTrace();
-             		  }           
-        	done = true;
         }else if(Answer.equals("new game") || Answer.equals("new")){
         	System.out.println("A new game will be started");
-        	done = true;
+        	Game game = new Game();
+        	return game;
+        }else{
+        	System.out.println("That is an Incorrect input.");
         }
 		}
+		return null;
 		
-		return done;
 	}
+	
+	private static Game readObjects() throws FileNotFoundException {
+		Game deserializedGame = null;
+		try {
+            FileInputStream fileIn = new FileInputStream("data/saveFile.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            deserializedGame = (Game) in.readObject();
+            in.close();
+            fileIn.close();
+            return deserializedGame;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+		return deserializedGame;
+		
+	}
+
 	}
--- Zork Project/src/com/bayviewglen/ZorkProject/Zork.java
+++ Zork Project/src/com/bayviewglen/ZorkProject/Zork.java
@@ -1,16 +1,38 @@
 package com.bayviewglen.ZorkProject;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Zork {
+
+public class Zork implements Serializable{
 	private static Intro intro ;
 	
-	public static void main(String [] args){
+	public static void main(String [] args) throws FileNotFoundException{
 		
 		intro = new Intro();
-		Game game = new Game();
+		Game game;
 		System.out.println("Welcome to Chippys Cake Adventure (based off the events of a true story aka portal 2");
-		intro.gameIntro();
-		game.play();
+		game = intro.gameIntro();
+		boolean save = game.play();
+		if(save)
+		saveGame(game);
+	}
+
+	private static void saveGame(Game game) {
+
+		try {
+			FileOutputStream fileOut = new FileOutputStream("data/saveFile.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(game);
+			out.close();
+			fileOut.close();
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+
 	}
 
 }
